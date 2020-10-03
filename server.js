f66b2621cb40e15aaa7e8a397ca7fcfe44a56b51
Chris Myers <myerschristopher@johndeere.com>
2020-10-03 10:15:40 -0500

    setup project folders, server.js

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const express = require('express');
+const methodOverride = require('method-override');
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
+
+// const routes = require('./routes');
+const app = express();
+
+const User = require('./models').User;
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+    let token = req.cookies.jwt;
+
+    console.log(`Token: ${token}`);
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+        if(err || !decodedUser) {
+            return res.send(err);
+        }
+        req.user = decodedUser;
+
+        next();
+    });
+}
+
+// new routes
+// app.use('/fruits', verifyToken, routes.fruits);
+// app.use('/users', verifyToken, routes.users);
+// app.use('/auth', routes.auth);
+
+// splash page
+// app.get('/', (req, res) => {
+//     User.findAll()
+//     .then(users => {
+//         res.render('users/index.ejs', {
+//             users: users
+//         });
+//     })
+// });
+
+app.listen(process.env.PORT, () => {
+    console.log(`I am listening on port ${process.env.PORT} whats up?`);
+});
