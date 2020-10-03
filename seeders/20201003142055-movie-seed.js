3fe285ec7aee4ac2c121fc1ca807051e422d1662
Chris Myers <myerschristopher@johndeere.com>
2020-10-03 09:47:21 -0500

    update data models

---
+++ seeders/20201003142055-movie-seed.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkInsert('Movies', [
+      {
+        imdbId: "tt0078748",
+        title: "Alien",
+        img: "https://m.media-amazon.com/images/M/MV5BMmQ2MmU3NzktZjAxOC00ZDZhLTk4YzEtMDMyMzcxY2IwMDAyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+        releaseYear: 1979,
+        director: "Ridley Scott",
+        genre: "Horror, Sci-Fi",
+        plot: "After a space merchant vessel receives an unknown transmission as a distress call, one of the crew is attacked by a mysterious life form and they soon realize that its life cycle has merely begun."
+      },
+    ], {});
+  },
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkDelete('Movies', null, {});
+  }
+};
