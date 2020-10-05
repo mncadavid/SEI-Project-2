be664cd0d243c382593ade0de3c2b07e5bc39c15
Chris Myers <myerschristopher@johndeere.com>
2020-10-05 10:13:48 -0500

    movie db search and api call with error message

--- controllers/movies.js
+++ controllers/movies.js
@@ -23,24 +23,40 @@ const searchForMovie = (req, res) => {
         console.log(movies);
         
         // if found, return and 
-        if(movies) {
+        if(movies.length != 0) {
             // render page with movies information
             res.render('movies/index.ejs', {
                 movies: movies
             });
         } else {
             // if not found make API call
-
-            // if found in api call add to db
-                // create call add short info from results to db
-                    // then search for movies in movies table
-                        // if found
-                            // render page
-    
-                        // if not found error has occurred
-    
-            // not found 
-            // return not found message
+            axios({
+                url: `http://www.omdbapi.com/?s=${req.body.title}&type=movie&apikey=${process.env.OMDB_API_KEY}`,
+                method: 'get'
+            }).then((response) => {
+                const foundMovies = response.data.Search;
+                console.log(foundMovies);
+                // if found in api call add to db
+                if(foundMovies != undefined) {
+                    // create call add short info from results to db
+                        // then search for movies in movies table
+                            // if found
+                                // render page
+        
+                            // if not found error has occurred
+                    res.render('movies/index.ejs', {
+                        movies: foundMovies
+                    });
+                } else {
+                    // return not found message
+                    res.render('movies/index.ejs', {
+                        message: `Unable to find any movies matching ${req.body.title}`
+                    });
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            });
         }
     })
     .catch (err => {
@@ -50,28 +66,7 @@ const searchForMovie = (req, res) => {
 
 
 
-    // axios({
-    //     url: `http://www.omdbapi.com/?s=${req.body.title}&type=movie&apikey=${process.env.OMDB_API_KEY}`,
-    //     method: 'get'
-    // }).then((response) => {
-    //     const foundMovies = response.data.Search;
-    //     console.log(foundMovies);
 
-    //     for( let i = 0 ; i < foundMovies.length ; i ++ ) {
-
-    //     }
-
-
-
-
-    //     if(response.data) {
-    //         res.render('movies/index.ejs', {
-    //             movies: foundMovies
-    //         });
-    //     }
-    // }).catch((err) => {
-    //     console.error(err);
-    // });
 
 }
 
