2bb31a5928877a606ae17607f334aa6bc01bcdc4
Chris Myers <myerschristopher@johndeere.com>
2020-10-07 13:33:03 -0500

    search the web button

--- routes/movies.js
+++ routes/movies.js
@@ -7,6 +7,7 @@ router.get('/', ctrl.movies.renderSearchPage);
 router.get('/:imdbID', ctrl.movies.renderMovieShowPage);
 
 router.put('/search', ctrl.movies.searchForMovie);
+router.post('/search/web', ctrl.movies.searchforMovieWeb);
 
 
 module.exports = router;
